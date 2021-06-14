7831bb5cf123b723d7c0aac2e14f570032d1bb27
Sam Schick <schicks448@gmail.com>
2021-06-14 12:04:06 -0400

    initial commit

---
+++ src/VegaLite.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import embed from 'vega-embed'
+  import type {Result, VisualizationSpec} from 'vega-embed'
+
+  let element: HTMLElement | undefined = undefined
+  export let spec: VisualizationSpec
+  let graph: Result | undefined = undefined
+
+  onMount(async () => {
+    if (element) {
+      graph = await embed(element, spec)
+    }
+  })
+</script>
+
+<div bind:this={element}></div>
